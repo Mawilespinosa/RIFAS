4be09434c83d6ce9fe17a6616c2ccf07ffd11af1
manuel <mawilespinosa@gmail.com>
2023-06-29 14:10:06 -0500

    First commit

---
+++ Código.js
@@ -0,0 +1,36 @@
+let HOJA = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+
+function doGet(){
+  let web = HtmlService.createTemplateFromFile('index').evaluate();
+  return web
+}
+function doPost(){
+  let web = HtmlService.createTemplateFromFile('index').evaluate();
+  return web
+}
+function obtenerWeb(nombre){
+  return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+function numerosDisponibles(){
+  let range = HOJA.getRange("A2:A101").getValues();
+  return range;
+}
+function guardarDatos(data){ 
+  let ult =  HOJA.getRange("C"+(HOJA.getLastRow()+1)).getNextDataCell(SpreadsheetApp.Direction.UP).getRow()+1;  
+  
+  
+  let numero = data.numero;
+  let nombre = data.nombre;
+  let apellido = data.apellido;
+  let celular = data.celular;
+  let correo = data.correo;
+
+  // Crea un arreglo con los datos
+  let datos = [[numero, nombre, apellido, correo, celular]];
+
+  // Define el rango en el que deseas pegar los datos (C2:G2)
+  let rango = HOJA.getRange("C" + ult + ":g" + ult);
+
+  // Pega los datos en el rango especificado
+  rango.setValues(datos);
+}
